d63c8389a3ac8f570fb886102cab0d4baee38ef7
Jamie McCarthy <jamie@mccarthy.vg>
2013-07-26 17:47:17 -0400

    cloud-setup.sh

--- cbs.sh
+++ cbs.sh
@@ -12,5 +12,5 @@ HOME_DIR=/home/$USER
 SRC_DIR=$HOME_DIR/src
 SETUP_DIR=$SRC_DIR/$THIS_PROJECT
 
-ssh -oStrictHostKeyChecking=no -i ~/.ssh/id_hpcloud $USER@$IP "touch $HOME_DIR/.bash_history && sudo apt-get -yqq update && sudo apt-get -yqq install git && mkdir -p $SRC_DIR && rm -rf $SETUP_DIR && git clone https://github.com/jamiemccarthy/$THIS_PROJECT.git $SETUP_DIR && $SETUP_DIR/cloud-setup.sh"
+ssh -oStrictHostKeyChecking=no -i ~/.ssh/id_hpcloud $USER@$IP "touch $HOME_DIR/.bash_history && sudo apt-get -yqq update && sudo apt-get -yqq install git && mkdir -p $SRC_DIR && rm -rf $SETUP_DIR && git clone -q https://github.com/jamiemccarthy/$THIS_PROJECT.git $SETUP_DIR && $SETUP_DIR/cloud-setup.sh"
 
--- cloud-setup.sh
+++ cloud-setup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo cs
+
+# "Cloud setup"
+#
+# Set up a new cloud machine to run devstack
+
+USER=ubuntu
+JHS_PROJECT=jamie-hpcloud-setup
+
+HOME_DIR=/home/$USER
+SRC_DIR=$HOME_DIR/src
+DEVSTACK_DIR=$SRC_DIR/devstack
+JHS_DIR=$SRC_DIR/$JHS_PROJECT
+
+git clone -q http://github.com/openstack-dev/devstack.git $DEVSTACK_DIR
+cp -a $JHS_DIR/localrc $DEVSTACK_DIR/localrc
+cd $DEVSTACK_DIR
+./stack.sh > stack.sh.out 2> stack.sh.err
+
