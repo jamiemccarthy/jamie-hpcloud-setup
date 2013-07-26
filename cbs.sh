d83399da9dea36dd43f862b4d1c763e8d295b737
Jamie McCarthy <jamie@mccarthy.vg>
2013-07-26 16:55:39 -0400

    Typofix

--- cbs.sh
+++ cbs.sh
@@ -10,5 +10,5 @@ SRC_DIR=/home/$USER/src
 THIS_PROJECT=jamie-hpcloud-setup
 SETUP_DIR=$SRC_DIR/$THIS_PROJECT
 
-ssh -oStrictHostKeyChecking=no -i ~/.ssh/id_hpcloud $USER@$IP 'sudo apt-get update && sudo apt-get install git && mkdir -p $SRC_DIR && git clone https://github.com/jamiemccarthy/$THIS_PROJECT.git $SETUP_DIR && $SETUP_DIR/cloud-setup.sh'
+ssh -oStrictHostKeyChecking=no -i ~/.ssh/id_hpcloud $USER@$IP "sudo apt-get update && sudo apt-get install git && mkdir -p $SRC_DIR && git clone https://github.com/jamiemccarthy/$THIS_PROJECT.git $SETUP_DIR && $SETUP_DIR/cloud-setup.sh"
 
