11db1336befa8e75b97ff259d12026b2f67d5f85
Jamie McCarthy <jamie@mccarthy.vg>
2013-08-06 15:41:37 -0400

    Still working

--- cloud-setup.sh
+++ cloud-setup.sh
@@ -5,12 +5,12 @@
 # Set up a new cloud machine to run devstack
 
 USER=ubuntu
-JHS_PROJECT=jamie-hpcloud-setup
+JAMIE_SETUP_PROJECT=jamie-hpcloud-setup
 
 HOME_DIR=/home/$USER
 SRC_DIR=$HOME_DIR/src
 DEVSTACK_DIR=$SRC_DIR/devstack
-JHS_DIR=$SRC_DIR/$JHS_PROJECT
+JAMIE_SETUP_DIR=$SRC_DIR/$JAMIE_SETUP_PROJECT
 
 # Try to undo installation before redo. May or may not work.
 
@@ -24,9 +24,12 @@ if [ ! -d $DEVSTACK_DIR ]; then
 	git clone -q http://github.com/openstack-dev/devstack.git $DEVSTACK_DIR
 fi
 git --git-dir=$DEVSTACK_DIR/.git --work-tree=$DEVSTACK_DIR pull origin master
-cp -a $JHS_DIR/localrc $DEVSTACK_DIR/localrc
+# My localrc sets all devstack's passwords to "1"
+cp -a $JAMIE_SETUP_DIR/localrc $DEVSTACK_DIR/localrc
 cd $DEVSTACK_DIR
 ./stack.sh > stack.sh.out 2> stack.sh.err
+
+# Install barbican, per <https://github.com/cloudkeep/barbican/wiki/Developer-Guide>
 
 sudo apt-get -yqq install python-virtualenv libsqlite3-dev
 cd $SRC_DIR
@@ -35,13 +38,13 @@ cd barbican
 virtualenv .venv
 source .venv/bin/activate
 pip install uwsgi
-pip install paste
 pip install -r tools/pip-requires
 pip install -r tools/test-requires
-cp -a etc/barbican/barbican-api.conf ~/ # TODO review how barbiban-all does this if the file is missing
+cp -a etc/barbican/barbican-api.conf ~/
 sudo mkdir /var/lib/barbican ; sudo chown ubuntu:ubuntu /var/lib/barbican
 sudo mkdir /var/log/barbican ; sudo chown ubuntu:ubuntu /var/log/barbican
 sudo mkdir /etc/barbican     ; sudo chown ubuntu:ubuntu /etc/barbican
 cp etc/barbican/barbican-{api,admin}-paste.ini /etc/barbican/
+sudo updatedb # I find "locate" useful
 bin/barbican-all
 
