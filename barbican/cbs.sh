c6ea52c3030b052c5e32a583a31df330ed3c95cc
Jamie McCarthy <jamie@mccarthy.vg>
2013-09-18 16:02:32 -0400

    barbican/ subdirectory

--- barbican/cbs.sh
+++ barbican/cbs.sh
@@ -14,7 +14,7 @@ SETUP_DIR=$SRC_DIR/$THIS_PROJECT
 
 # TODO? PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
-ssh -oStrictHostKeyChecking=no -i ~/.ssh/id_hpcloud $USER@$IP "touch $HOME_DIR/.bash_history && sudo apt-get -yqq update && sudo apt-get -yqq install git && mkdir -p $SRC_DIR && rm -rf $SETUP_DIR && git clone -q https://github.com/jamiemccarthy/$THIS_PROJECT.git $SETUP_DIR && $SETUP_DIR/cloud-setup.sh"
+ssh -oStrictHostKeyChecking=no -i ~/.ssh/id_hpcloud $USER@$IP "touch $HOME_DIR/.bash_history && sudo apt-get -yqq update && sudo apt-get -yqq install git && mkdir -p $SRC_DIR && rm -rf $SETUP_DIR && git clone -q https://github.com/jamiemccarthy/$THIS_PROJECT.git $SETUP_DIR && $SETUP_DIR/barbican/cloud-setup.sh"
 
 # Worth considering
 # $ sudo apt-get install python-software-properties
